7ecd9f63f9b8faf2437db9ed8b483952ae572168
thembones79 <michal.szumnarski@gmail.com>
2020-01-18 09:48:37 +0100

    added sass

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Weather from "./Weather";
+
+const App = () => {
+  return <Weather />;
+};
+
+export default App;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "./Weather.scss";
 
-const Weather = () => {
+const Weather = ({ isLoading }) => {
   if (isLoading) {
     return <div>Loading...SPINNER</div>;
   }
+
+  return (
+    <div className="weather">
+      <div>This is the weather app</div>
+    </div>
+  );
 };
+
+export default Weather;
